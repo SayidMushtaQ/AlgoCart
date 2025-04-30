d2beb6051775fca61f130c3a37cf5273a0d68097
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-04-30 14:37:34 +0530

    adding data

--- src/data/ProductData.js
+++ src/data/ProductData.js
@@ -1,6 +1,502 @@
-export const productsData = new Array(30).fill(null).map((_, i) => ({
-    id: i + 1,
-    name: `Mobile Phone ${i + 1}`,
-    price: Math.floor(Math.random() * 50000) + 5000,
-    image: `https://via.placeholder.com/200x200?text=Phone+${i + 1}`,
-  }));
+export const productsData = [
+  {
+    "id": 1,
+    "name": "OnePlus Phone Model 57",
+    "price": 24916,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "128GB",
+    "Battery": "5000mAh",
+    "rating": 3.1
+  },
+  {
+    "id": 2,
+    "name": "Oppo Phone Model 23",
+    "price": 35089,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Oppo",
+    "Storage": "256GB",
+    "Battery": "3000mAh",
+    "rating": 4.0
+  },
+  {
+    "id": 3,
+    "name": "OnePlus Phone Model 63",
+    "price": 11726,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "512GB",
+    "Battery": "6000mAh",
+    "rating": 4.2
+  },
+  {
+    "id": 4,
+    "name": "Apple Phone Model 66",
+    "price": 76547,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Apple",
+    "Storage": "256GB",
+    "Battery": "3000mAh",
+    "rating": 3.7
+  },
+  {
+    "id": 5,
+    "name": "OnePlus Phone Model 35",
+    "price": 98911,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "128GB",
+    "Battery": "5000mAh",
+    "rating": 4.5
+  },
+  {
+    "id": 6,
+    "name": "Nokia Phone Model 15",
+    "price": 25905,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Nokia",
+    "Storage": "64GB",
+    "Battery": "4500mAh",
+    "rating": 3.9
+  },
+  {
+    "id": 7,
+    "name": "Xiaomi Phone Model 96",
+    "price": 26789,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Xiaomi",
+    "Storage": "512GB",
+    "Battery": "5000mAh",
+    "rating": 4.2
+  },
+  {
+    "id": 8,
+    "name": "Realme Phone Model 12",
+    "price": 73306,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Realme",
+    "Storage": "64GB",
+    "Battery": "5000mAh",
+    "rating": 4.8
+  },
+  {
+    "id": 9,
+    "name": "OnePlus Phone Model 82",
+    "price": 74537,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "512GB",
+    "Battery": "5000mAh",
+    "rating": 3.9
+  },
+  {
+    "id": 10,
+    "name": "Apple Phone Model 23",
+    "price": 83570,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Apple",
+    "Storage": "512GB",
+    "Battery": "4500mAh",
+    "rating": 3.1
+  },
+  {
+    "id": 11,
+    "name": "OnePlus Phone Model 82",
+    "price": 69583,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "256GB",
+    "Battery": "3000mAh",
+    "rating": 4.5
+  },
+  {
+    "id": 12,
+    "name": "Apple Phone Model 67",
+    "price": 94882,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Apple",
+    "Storage": "128GB",
+    "Battery": "4500mAh",
+    "rating": 4.6
+  },
+  {
+    "id": 13,
+    "name": "Xiaomi Phone Model 63",
+    "price": 8097,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Xiaomi",
+    "Storage": "256GB",
+    "Battery": "6000mAh",
+    "rating": 5.0
+  },
+  {
+    "id": 14,
+    "name": "Realme Phone Model 8",
+    "price": 81636,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Realme",
+    "Storage": "512GB",
+    "Battery": "6000mAh",
+    "rating": 4.7
+  },
+  {
+    "id": 15,
+    "name": "Realme Phone Model 73",
+    "price": 79055,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Realme",
+    "Storage": "128GB",
+    "Battery": "6000mAh",
+    "rating": 4.8
+  },
+  {
+    "id": 16,
+    "name": "Oppo Phone Model 43",
+    "price": 72454,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Oppo",
+    "Storage": "64GB",
+    "Battery": "6000mAh",
+    "rating": 4.8
+  },
+  {
+    "id": 17,
+    "name": "Realme Phone Model 89",
+    "price": 93131,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Realme",
+    "Storage": "256GB",
+    "Battery": "6000mAh",
+    "rating": 4.5
+  },
+  {
+    "id": 18,
+    "name": "Xiaomi Phone Model 25",
+    "price": 60759,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Xiaomi",
+    "Storage": "512GB",
+    "Battery": "4000mAh",
+    "rating": 4.2
+  },
+  {
+    "id": 19,
+    "name": "Xiaomi Phone Model 62",
+    "price": 61479,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Xiaomi",
+    "Storage": "256GB",
+    "Battery": "3000mAh",
+    "rating": 4.5
+  },
+  {
+    "id": 20,
+    "name": "Google Phone Model 86",
+    "price": 69960,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Google",
+    "Storage": "128GB",
+    "Battery": "4000mAh",
+    "rating": 3.7
+  },
+  {
+    "id": 21,
+    "name": "Samsung Phone Model 51",
+    "price": 29497,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Samsung",
+    "Storage": "64GB",
+    "Battery": "6000mAh",
+    "rating": 3.5
+  },
+  {
+    "id": 22,
+    "name": "Nokia Phone Model 32",
+    "price": 17371,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Nokia",
+    "Storage": "512GB",
+    "Battery": "3000mAh",
+    "rating": 3.2
+  },
+  {
+    "id": 23,
+    "name": "Nokia Phone Model 13",
+    "price": 65822,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Nokia",
+    "Storage": "128GB",
+    "Battery": "4500mAh",
+    "rating": 3.0
+  },
+  {
+    "id": 24,
+    "name": "Google Phone Model 99",
+    "price": 98378,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Google",
+    "Storage": "256GB",
+    "Battery": "3000mAh",
+    "rating": 4.0
+  },
+  {
+    "id": 25,
+    "name": "Realme Phone Model 88",
+    "price": 78331,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Realme",
+    "Storage": "512GB",
+    "Battery": "4000mAh",
+    "rating": 4.2
+  },
+  {
+    "id": 26,
+    "name": "Samsung Phone Model 62",
+    "price": 33261,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Samsung",
+    "Storage": "128GB",
+    "Battery": "3000mAh",
+    "rating": 3.4
+  },
+  {
+    "id": 27,
+    "name": "Samsung Phone Model 85",
+    "price": 67662,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Samsung",
+    "Storage": "64GB",
+    "Battery": "6000mAh",
+    "rating": 4.6
+  },
+  {
+    "id": 28,
+    "name": "Apple Phone Model 35",
+    "price": 49183,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Apple",
+    "Storage": "512GB",
+    "Battery": "6000mAh",
+    "rating": 4.4
+  },
+  {
+    "id": 29,
+    "name": "Xiaomi Phone Model 3",
+    "price": 17443,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Xiaomi",
+    "Storage": "64GB",
+    "Battery": "3000mAh",
+    "rating": 3.1
+  },
+  {
+    "id": 30,
+    "name": "Vivo Phone Model 33",
+    "price": 44149,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Vivo",
+    "Storage": "64GB",
+    "Battery": "5000mAh",
+    "rating": 3.9
+  },
+  {
+    "id": 31,
+    "name": "Nokia Phone Model 91",
+    "price": 61035,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Nokia",
+    "Storage": "256GB",
+    "Battery": "5000mAh",
+    "rating": 4.0
+  },
+  {
+    "id": 32,
+    "name": "Google Phone Model 62",
+    "price": 57646,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Google",
+    "Storage": "256GB",
+    "Battery": "4000mAh",
+    "rating": 4.3
+  },
+  {
+    "id": 33,
+    "name": "Oppo Phone Model 70",
+    "price": 11335,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Oppo",
+    "Storage": "256GB",
+    "Battery": "4500mAh",
+    "rating": 3.0
+  },
+  {
+    "id": 34,
+    "name": "OnePlus Phone Model 81",
+    "price": 66080,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "256GB",
+    "Battery": "3000mAh",
+    "rating": 4.7
+  },
+  {
+    "id": 35,
+    "name": "Motorola Phone Model 44",
+    "price": 77230,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Motorola",
+    "Storage": "64GB",
+    "Battery": "5000mAh",
+    "rating": 4.6
+  },
+  {
+    "id": 36,
+    "name": "Oppo Phone Model 98",
+    "price": 13041,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Oppo",
+    "Storage": "256GB",
+    "Battery": "4000mAh",
+    "rating": 4.4
+  },
+  {
+    "id": 37,
+    "name": "Nokia Phone Model 90",
+    "price": 52391,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Nokia",
+    "Storage": "256GB",
+    "Battery": "4500mAh",
+    "rating": 4.7
+  },
+  {
+    "id": 38,
+    "name": "Samsung Phone Model 4",
+    "price": 63820,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Samsung",
+    "Storage": "64GB",
+    "Battery": "3000mAh",
+    "rating": 4.0
+  },
+  {
+    "id": 39,
+    "name": "Vivo Phone Model 52",
+    "price": 85032,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Vivo",
+    "Storage": "256GB",
+    "Battery": "6000mAh",
+    "rating": 4.8
+  },
+  {
+    "id": 40,
+    "name": "OnePlus Phone Model 63",
+    "price": 64505,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "128GB",
+    "Battery": "5000mAh",
+    "rating": 3.7
+  },
+  {
+    "id": 41,
+    "name": "Xiaomi Phone Model 73",
+    "price": 53239,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Xiaomi",
+    "Storage": "256GB",
+    "Battery": "6000mAh",
+    "rating": 4.0
+  },
+  {
+    "id": 42,
+    "name": "Nokia Phone Model 71",
+    "price": 15448,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Nokia",
+    "Storage": "128GB",
+    "Battery": "4500mAh",
+    "rating": 4.2
+  },
+  {
+    "id": 43,
+    "name": "Vivo Phone Model 77",
+    "price": 26483,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Vivo",
+    "Storage": "512GB",
+    "Battery": "4000mAh",
+    "rating": 4.8
+  },
+  {
+    "id": 44,
+    "name": "Vivo Phone Model 38",
+    "price": 89565,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Vivo",
+    "Storage": "512GB",
+    "Battery": "4500mAh",
+    "rating": 4.0
+  },
+  {
+    "id": 45,
+    "name": "OnePlus Phone Model 54",
+    "price": 58874,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "128GB",
+    "Battery": "6000mAh",
+    "rating": 4.2
+  },
+  {
+    "id": 46,
+    "name": "Samsung Phone Model 43",
+    "price": 50980,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Samsung",
+    "Storage": "128GB",
+    "Battery": "3000mAh",
+    "rating": 3.0
+  },
+  {
+    "id": 47,
+    "name": "OnePlus Phone Model 93",
+    "price": 77981,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "OnePlus",
+    "Storage": "128GB",
+    "Battery": "3000mAh",
+    "rating": 3.0
+  },
+  {
+    "id": 48,
+    "name": "Xiaomi Phone Model 65",
+    "price": 34426,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Xiaomi",
+    "Storage": "256GB",
+    "Battery": "3000mAh",
+    "rating": 4.6
+  },
+  {
+    "id": 49,
+    "name": "Xiaomi Phone Model 95",
+    "price": 92021,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Xiaomi",
+    "Storage": "512GB",
+    "Battery": "4000mAh",
+    "rating": 4.5
+  },
+  {
+    "id": 50,
+    "name": "Nokia Phone Model 94",
+    "price": 26783,
+    "image": "https://res.cloudinary.com/ddpvlh9he/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1746003789/713SsA7gftL._AC_UF1000_1000_QL80__w5bek7.jpg",
+    "Brand": "Nokia",
+    "Storage": "512GB",
+    "Battery": "4500mAh",
+    "rating": 3.5
+  }
+]
